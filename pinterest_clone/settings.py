00cc39dde8bad7b133865dede2995af939535d39
keval <keval.inexture>
2022-06-13 10:16:11 +0530

    changing static image path

--- pinterest_clone/settings.py
+++ pinterest_clone/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pinterestclone1.herokuapp.com']
+ALLOWED_HOSTS = ['pinterestclone1.herokuapp.com','127.0.0.1']
 
 
 # Application definition
